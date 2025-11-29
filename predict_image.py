01b17644448cd1c9866a927496b1afd86b74ccb9
sidhvi-komali <sidhvi.siddharth@gmail.com>
2025-11-28 20:37:21 -0800

    Fix fire confidence threshold condition

--- predict_image.py
+++ predict_image.py
@@ -54,7 +54,7 @@ def predict(image_path):
     # Binary model output → single probability (nofire)
     nofire_confidence = 1-fire_confidence
     
-    if fire_confidence <= FIRE_THRESHOLD:
+    if fire_confidence >= FIRE_THRESHOLD:
         label = "🔥 Fire"
         prob = fire_confidence
     else:
